830ff6c960a07b02ec5302b06fca3d0fd498cd88
Michael Okoe <michael.okoejr@gmail.com>
2022-08-23 02:36:14 +0000

    commit tree 53d83bb8865b0d37e7c5fdebab71af30668ee5c0

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
+  # 'name' setter
+  def name=(name)
+    @name = name
+  end 
+
+  # 'name' getter
+  def name
+    @name
+  end 
+
+# Creating a 'setter' and 'getter' methods for breed instance
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
 
 end
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+  # Creating a 'name' setter and getter
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+    # Defining a 'job' setter and getter
+    def job=(job)
+      @job = job
+    end
+
+    def job
+      @job
+    end 
 
 end
